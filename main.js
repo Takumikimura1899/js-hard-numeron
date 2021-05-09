322b195f6b52386778cf047e5bffab50371a2e56
Takumikimura141899 <kimura141899@gmail.com>
2021-05-09 22:27:09 +0900

    challange-finish

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// // まずは答えになる3桁の数字を作成しよう！
+// まずは答えになる3桁の数字を作成しよう！
 
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -10,20 +10,20 @@ for (let i = arraylength - 1; i >= 0; i--) {
 }
 
 console.log(array[0], array[1], array[2]);
+// ↑↑↑配列のシャッフル完了↑↑↑
 
 let cpNum1 = array[0];
 let cpNum2 = array[1];
 let cpNum3 = array[2];
 
-// console.log(cpNum1);
-// console.log(typeof cpNum1);
-
 let eatCount = 0;
 let biteCount = 0;
+let checkCountDown = 10;
 
 //入力したボタンのvalueを取得する
 const answerNum = document.getElementById("answerNum");
 const numCheck = document.getElementById("numCheck");
+const remainTurn = document.getElementById("remainTurn");
 
 // 答え合わせボタンを押した時の処理
 numCheck.addEventListener("click", () => {
@@ -32,18 +32,17 @@ numCheck.addEventListener("click", () => {
 
   console.log(answerNum.value);
   const numArray = answerNum.value;
-  //  [...answerNum.value];
-  console.log(numArray);
   console.log(numArray[0], numArray[1], numArray[2]);
   let answerNum1 = numArray[0];
   let answerNum2 = numArray[1];
   let answerNum3 = numArray[2];
 
+  //   三桁の数字以外はアラート
   if (numArray.length > 3 || numArray.length < 3) {
     alert("三桁の数字を入力してください");
     return;
   }
-
+  // 同じ数字はアラート
   if (
     answerNum1 == answerNum2 ||
     answerNum2 == answerNum3 ||
@@ -52,9 +51,6 @@ numCheck.addEventListener("click", () => {
     alert("同じ数字は使えません");
     return;
   }
-  //   console.log(answerNum1);
-  //   console.log(answerNum2);
-  //   console.log(typeof answerNum1);
 
   //   answerNum1とcpnum1,2,3の比較
   if (answerNum1 == cpNum1) {
@@ -91,9 +87,17 @@ numCheck.addEventListener("click", () => {
   console.log(eatCount);
   console.log(biteCount);
 
+  //   eatbiteのアラート
   if (eatCount === 3) {
     alert("おめおめ");
+    window.location.reload();
   } else {
     alert(eatCount + "EAT" + biteCount + "BITE");
+  }
+  checkCountDown--;
+  remainTurn.textContent = "あと" + checkCountDown + "回";
+  if (checkCountDown === 0) {
+    remainTurn.textContent =
+      "残念。答えは、" + [cpNum1] + [cpNum2] + [cpNum3] + "でした。";
   }
 });
